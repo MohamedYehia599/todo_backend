2922446302e1b963fe697d92b2302b238c955de9
MohamedYehia599 <mohamed.yehia3331@gmail.com>
2022-06-19 21:30:24 +0200

    task_unit_test

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -13,10 +13,53 @@ class TaskTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+
+
+
+        public function test_task_index()
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/tasks');
 
         $response->assertStatus(200);
     }
+
+
+    public function test_task_show()
+    {
+        $response = $this->get('/api/tasks/10');
+
+        $response->assertStatus(200);
+    }
+
+
+    public function test_task_store(){
+
+
+        $response = $this->post('/api/tasks',[
+            'title'=>'test_title_createe',
+            'description'=>'test_description',
+            'user_id'=>10
+        ]);
+        $response->assertStatus(201);
+    }
+    public function test_task_update(){
+
+
+        $response = $this->put('/api/tasks/10',[
+            'title'=>'test_title_updatee',
+            'description'=>'test_description',
+            'user_id'=>10
+        ]);
+        $response->assertStatus(200);
+    }
+
+    public function test_task_delete(){
+        $response = $this->delete('/api/tasks/30');
+        $response->assertStatus(200);
+    }
+
+    
+
+
+
 }
